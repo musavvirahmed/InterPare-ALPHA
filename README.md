8e217c5680eb2b2a816dff089a0bbca70fc15fd4
musavvirahmed <musavvir@gmail.com>
2019-03-19 09:42:36 +0800

    Added vm2

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # UI Diff Tool
+
+An internal UI screenshop comparison tool based on zurb's twentytwenty plguin.
--- js/custom.js
+++ js/custom.js
@@ -1,19 +1,19 @@
   $(window).on('load', function(){
-    $("#comparison1").twentytwenty({
+    $("#comparison1vm1,#comparison2vm1").twentytwenty({
     default_offset_pct: 0.6, // How much of the before image is visible when the page loads
     orientation: 'horizontal', // Orientation of the before and after images ('horizontal' or 'vertical')
     // no_overlay: true, //Do not show the overlay with before and after
     before_label: 'Current / Old Design', // Set a custom before label
-    after_label: 'New Design', // Set a custom after label
+    after_label: 'New Design VM1', // Set a custom after label
   });
 });
 
 $(window).on('load', function(){
-  $("#comparison2").twentytwenty({
+  $("#comparison1vm2,#comparison2vm2").twentytwenty({
   default_offset_pct: 0.6, // How much of the before image is visible when the page loads
   orientation: 'horizontal', // Orientation of the before and after images ('horizontal' or 'vertical')
   // no_overlay: true, //Do not show the overlay with before and after
   before_label: 'Current / Old Design', // Set a custom before label
-  after_label: 'New Design', // Set a custom after label
+  after_label: 'New Design VM2', // Set a custom after label
 });
 });
